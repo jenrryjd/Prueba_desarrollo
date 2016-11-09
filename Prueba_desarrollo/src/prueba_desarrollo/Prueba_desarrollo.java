a554efb353511428fc821fc2ddecc2b3c4a1bda9
12-LAB6 <12-LAB6@172.21.106.27>
2016-11-09 11:59:15 -0500

    mensaje 1

--- Prueba_desarrollo/src/prueba_desarrollo/Prueba_desarrollo.java
+++ Prueba_desarrollo/src/prueba_desarrollo/Prueba_desarrollo.java
@@ -5,6 +5,8 @@
  */
 package prueba_desarrollo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 12-LAB6
@@ -15,7 +17,7 @@ public class Prueba_desarrollo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"sadasdasd");
     }
     
 }
